c58fdf93d7f37e1c0c3de1a55092de9ac5e41479
lilbutsa <james.walsh@phys.unsw.edu.au>
2024-03-24 18:50:42 -0700

    Updating Trace Generation for Mac

--- Jim_v7/Python_Programs/Begin_Here_Generate_Traces.py
+++ Jim_v7/Python_Programs/Begin_Here_Generate_Traces.py
@@ -11,7 +11,7 @@ from tkinter import filedialog
 from PIL import Image
 from multiprocessing import Pool
 
-sectionNumber = 12
+sectionNumber = 10
 # Sections
 
 # 0 - Import Parameters
@@ -44,65 +44,65 @@ displayMax = 95
 additionalExtensionsToRemove = 0  # remove extra .ome from working folder name if you want to
 
 # 2 - Split File into Individual Channels Parameters
-imStackMultipleFiles = False  # choose this if you're stack is split over multiple tiff files (i.e. >4Gb)
-imStackNumberOfChannels = 3  # Input the number of channels in the data
+imStackMultipleFiles = False   # choose this if you're stack is split over multiple tiff files (i.e. >4Gb)
+imStackNumberOfChannels = 1  # Input the number of channels in the data
 
-imStackDisableMetadata = False  # Images are usually split using embedded OME metadata but can be disabled if this causes problems
+imStackDisableMetadata = True   # Images are usually split using embedded OME metadata but can be disabled if this causes problems
 
 imStackStartFrame = 1  # Part of the image stack can be completely ignored for all downstream analysis, set to 1 to start from the first frame
 imStackEndFrame = -1  # Last frame to take. Negative numbers go from the end of the stack, so set to -1 to take the entire stack.
 
-imStackChannelsToTransform = '2 3'  # If no channels need to be transformed set channelsToTransform 
-imStackVerticalFlipChannel = '0 0'  # For each channel to be transformed put 1 to flip that channel or 0 to not. eg. '1 0' to flip channel 2 but not 3.
-imStackHorizontalFlipChannel = '1 0'  # Same as vertical
-imStackRotateChannel = '0 180'  # Rotate should either be 0, 90, 180 or 270 for the angle to rotate each selected channel
+imStackChannelsToTransform = ''  # If no channels need to be transformed set channelsToTransform 
+imStackVerticalFlipChannel = '1'  # For each channel to be transformed put 1 to flip that channel or 0 to not. eg. '1 0' to flip channel 2 but not 3.
+imStackHorizontalFlipChannel = '0'  # Same as vertical
+imStackRotateChannel = '0'  # Rotate should either be 0, 90, 180 or 270 for the angle to rotate each selected channel
 
 # 3 - Align Channels and Calculate Drifts Parameters
 alignIterations = 1  # Number of times to iterate drift correction calculations - 1 is fine if there minimal drift in the reference frames
 
-alignStartFrame = 12  # Select reference frames where there is signal in all channels at the same time start frame from 1
-alignEndFrame = 12
+alignStartFrame = 1  # Select reference frames where there is signal in all channels at the same time start frame from 1
+alignEndFrame = 5
 
-alignMaxShift = 30  # Limit the mamximum distance that the program will shift images for alignment this can help stop false alignments
+alignMaxShift = 10.00  # Limit the mamximum distance that the program will shift images for alignment this can help stop false alignments
 
 # Output the aligned image stacks. Note this is not required by JIM but can
 # be helpful for visualization. To save space, aligned stack will not output in batch
 # regarless of this value
-alignOutputStacks = False
+alignOutputStacks = False 
 
 # Parmeters for Automatic Alignment
-alignMaxIntensities = '65000 65000 65000'  # Set a threshold so that during channel to channel alignment agregates are ignored
-alignSNRCutoff = 0.20  # Set a minimum alignment SNR to throw warnings
+alignMaxIntensities = '65000 65000'  # Set a threshold so that during channel to channel alignment agregates are ignored
+alignSNRCutoff = 1.00  # Set a minimum alignment SNR to throw warnings
 
 # Multi Channel Alignment
-alignManually = False  # Manually set the alignment between the multiple channels, If set to false the program will try to automatically find an alignment
-alignXOffset = '-5 -5'
-alignYOffset = '-5 -5'
-alignRotationAngle = '-2 2'
-alignScalingFactor = '1 1'
+alignManually = False   # Manually set the alignment between the multiple channels, If set to false the program will try to automatically find an alignment
+alignXOffset = '0'
+alignYOffset = '0'
+alignRotationAngle = '0'
+alignScalingFactor = '1'
 
 # 4 - Make a SubAverage of the Image Stack for Detection Parameters
-detectUsingMaxProjection = False  # Use a max projection rather than mean. This is better for short lived blinking particles
+detectUsingMaxProjection = False   # Use a max projection rather than mean. This is better for short lived blinking particles
 
-detectionStartFrame = '1 -5 1'  # first frame of the reference region for detection for each channel
-detectionEndFrame = '5 -1 5'  # last frame of reference region. Negative numbers go from end of stack. i.e. -1 is last image in stack
-detectWeights = '1 1 1'  # Each channel is multiplied by this value before they're combined. This is handy if one channel is much brigthter than another.
+detectionStartFrame = '1'  # first frame of the reference region for detection for each channel
+detectionEndFrame = '25'  # last frame of reference region. Negative numbers go from end of stack. i.e. -1 is last image in stack
+detectWeights = '1'  # Each channel is multiplied by this value before they're combined. This is handy if one channel is much brigthter than another.
 
 # 5 - Detect Particles Parameters
 # Thresholding
-detectionCutoff = 1.00  # The cutoff for the initial thresholding. Typically in range 0.25-2
+detectionCutoff = 0.60  # The cutoff for the initial thresholding. Typically in range 0.25-2
 
 # Filtering
-detectLeftEdge = 5  # Excluded particles closer to the left edge than this. Make sure this value is larger than the maximum drift. 25 works well in most cases
-detectRightEdge = 5  # Excluded particles closer to the Right edge than this.
-detectTopEdge = 5  # Excluded particles closer to the Top edge than this.
-detectBottomEdge = 5  # Excluded particles closer to the Bottom edge than this.
+detectLeftEdge = 10  # Excluded particles closer to the left edge than this. Make sure this value is larger than the maximum drift. 25 works well in most cases
+detectRightEdge = 10  # Excluded particles closer to the Right edge than this.
+detectTopEdge = 10  # Excluded particles closer to the Top edge than this.
+detectBottomEdge = 10  # Excluded particles closer to the Bottom edge than this.
 
-detectMinCount = 0  # Minimum number of pixels in a ROI to be counted as a particle. Use this to exclude speckles of background
-detectMaxCount = 10000  # Maximum number of pixels in a ROI to be counted as a particle. Use this to exclude aggregates
+detectMinCount = 10  # Minimum number of pixels in a ROI to be counted as a particle. Use this to exclude speckles of background
+detectMaxCount = 100  # Maximum number of pixels in a ROI to be counted as a particle. Use this to exclude aggregates
 
 detectMinEccentricity = -0.10  # Eccentricity of best fit ellipse goes from 0 to 1 - 0
-detectMaxEccentricity = 1.1  # Use the maximum to exclude long, thin objects. Set it to a value above 1 to include long, thin objects
+detectMaxEccentricity = 1.10  # Use the maximum to exclude long, thin objects. Set it to a value above 1 to include long, thin objects
 
 detectMinLength = 0.00  # Minimum number of pixels for the major axis of the best fit ellipse
 detectMaxLength = 10000.00  # Maximum number of pixels for the major axis of the best fit ellipse
@@ -113,9 +113,9 @@ detectMinSeparation = -1000.00  # Minimum separation between ROI's. Given by the
 
 # 6 - Additional Background Detection Subaverage - Use this to detect all other particles that are not in the detection image to cut around for background
 
-additionBackgroundDetect = False  # enable the additional detection. Disable if all particles were detected (before filtering) above.
+additionBackgroundDetect = False   # enable the additional detection. Disable if all particles were detected (before filtering) above.
 
-additionBackgroundUseMaxProjection = True  # Use a max projection rather than mean. This is better for short lived blinking particles
+additionBackgroundUseMaxProjection = True   # Use a max projection rather than mean. This is better for short lived blinking particles
 
 additionalBackgroundStartFrame = '1 1'  # first frame of the reference region for background detection
 additionalBackgroundEndFrame = '-1 -1'  # last frame of background reference region. Negative numbers go from end of stack. i.e. -1 is last image in stack
@@ -131,7 +131,7 @@ expandBackInnerDist = 4.10  # Minimum distance to dilate beyond the ROI to measu
 expandBackOuterDist = 20.00  # Maximum distance to dilate beyond the ROI to measure the local background
 
 # 8 - Calculate Traces Parameter
-traceVerboseOutput = False  # Create additional file with additional statistics on each particle in each frame. Warning, this file can get very large. In general you don't want this.
+traceVerboseOutput = False   # Create additional file with additional statistics on each particle in each frame. Warning, this file can get very large. In general you don't want this.
 
 # 9 - View Traces Parameter
 montagePageNumber = 1  # Select the page number for traces. 28 traces per page. So traces from(n-1)*28+1 to n*28
@@ -139,9 +139,9 @@ montageTimePerFrame = 1 # Set to zero to just have frames
 montageTimeUnits = 'Frames'  # Unit to use for x axis
 
 # 10 - Extract Trace
-montageTraceNo = 9
+montageTraceNo = 80
 montageStart = 3
-montageEnd = 28
+montageEnd = 48
 montageDelta = 5
 montageAverage = 5
 
@@ -215,7 +215,7 @@ if sectionNumber == 0:
         splitLine = splitLine[1].split('#', 1)
         if len(splitLine) > 1:
             stringOut = stringOut + '  #' + splitLine[1]
-        print(stringOut)
+        #print(stringOut)
 
         programFileList[index[0]] = stringOut
 
@@ -328,6 +328,8 @@ if sectionNumber == 3:
     os.system(cmd)
 
     imin = Image.open(workingDir + "Alignment_Full_Projection_Before.tiff")
+    plt.figure('Before Drift Correction and Alignment')
+
     imred = np.zeros((imin.height, imin.width))
     imgreen = np.zeros((imin.height, imin.width))
     imblue = np.zeros((imin.height, imin.width))
@@ -340,8 +342,12 @@ if sectionNumber == 3:
         imgreen = imgreen + overlayColours[j][1] * iminnp
         imblue = imblue + overlayColours[j][2] * iminnp
     imout = np.dstack((imred, imgreen, imblue)).astype(np.uint8)
-    plt.figure('Before Drift Correction and Alignment')
-    plt.imshow(imout)
+
+    if imStackNumberOfChannels==1:
+        imout = iminnp
+        plt.imshow(imout, 'gray')
+    else:
+        plt.imshow(imout)
     plt.show()
 
     imin = Image.open(workingDir + "Alignment_Full_Projection_After.tiff")
@@ -357,8 +363,13 @@ if sectionNumber == 3:
         imgreen = imgreen + overlayColours[j][1] * iminnp
         imblue = imblue + overlayColours[j][2] * iminnp
     imout = np.dstack((imred, imgreen, imblue)).astype(np.uint8)
+
     plt.figure('After Drift Correction and Alignment')
-    plt.imshow(imout)
+    if imStackNumberOfChannels==1:
+        imout = iminnp
+        plt.imshow(imout, 'gray')
+    else:
+        plt.imshow(imout)
     plt.show()
 
 # 4- Make a SubAverage of the Image Stack for Detection
@@ -504,7 +515,7 @@ if sectionNumber == 8:
             cmd = cmd + ' -Verbose'
         os.system(cmd)
 
-    falsetrue = ['false', 'true'];
+    falsetrue = ['false', 'true']
     variableString = ('Date, ' + str(datetime.date.today()) +
                       '\nadditionalExtensionsToRemove,' + str(additionalExtensionsToRemove) +
                       '\nimStackMultipleFiles,' + falsetrue[int(imStackMultipleFiles)] +
@@ -575,33 +586,43 @@ if sectionNumber == 9:
 
     myColours = [[0, 0.447, 0.741], [0.85, 0.325, 0.098], [0.929, 0.694, 0.125], [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933] ,[0.635, 0.078, 0.184]]
     data = []
+    scalingFactors = []
     for j in range(imStackNumberOfChannels):
         channelFile = workingDir + "Channel_"+str(j+1)+"_Fluorescent_Intensities.csv"
         channelFile = list(csv.reader(open(channelFile)))
         channelFile.pop(0)
+        channelFile = np.array(channelFile).astype(float)
+        scalingFactors.append(np.ceil(np.log10(np.amax(channelFile)))-2)
         data.append(np.array(channelFile).astype(float))
 
-    plt.subplots(figsize=(17.78/2, 22.86/2), facecolor=(1, 1, 1))
-    xpoints = np.arange(0.0,len(data[0][0]))* montageTimePerFrame;
+    plt.subplots(7,4,figsize=(17.78/2.5, 22.86/2.5), facecolor=(1, 1, 1))
+    xpoints = np.arange(0.0,len(data[0][0])) * montageTimePerFrame
+    yaxisLabelStr = 'Normalised Intensity'
 
     for i in range(1, 29):
         if len(data[0]) > i + (montagePageNumber - 1) * 28:
             plt.subplot(7, 4, i)
-
-            for j in range(imStackNumberOfChannels):
-                toplot = data[j][i + (montagePageNumber - 1) * 28]
-                plt.plot(xpoints, toplot / max(toplot) , linewidth=2, color=myColours[j])
+            if imStackNumberOfChannels>1:
+                for j in range(imStackNumberOfChannels):
+                    toplot = data[j][i + (montagePageNumber - 1) * 28]
+                    plt.plot(xpoints, toplot / max(toplot), linewidth=2, color=myColours[j])
+                plt.ylim(-0.2, 1)
+            else:
+                toplot = data[0][i + (montagePageNumber - 1) * 28]
+                plt.plot(xpoints, toplot / pow(10, scalingFactors[0]), linewidth=2, color=myColours[0])
+                yaxisLabelStr = 'Intensity (10^'+str(int(scalingFactors[0]))+' a.u.)'
 
             plt.plot(plt.xlim(), [0, 0], color='black')
-            plt.ylim(-0.2, 1)
+
             xpos = round(float(measurements[i + (montagePageNumber - 1) * 28][0]))
             ypos = round(float(measurements[i + (montagePageNumber - 1) * 28][1]))
             plt.title('No. ' + str(i + (montagePageNumber - 1) * 28) + ' x ' + str(xpos) + ' y ' + str(ypos), fontsize = 10, weight='bold')
 
-    plt.annotate('Time ('+montageTimeUnits+')', xy=(0.5, 0), xycoords = 'figure fraction',fontsize = 14, weight='bold',horizontalalignment = 'center')
-    plt.annotate('Normalised Intensity', xy=(0, 0.5), xycoords='figure fraction', fontsize=14,
+    plt.annotate('Time ('+montageTimeUnits+')', xy=(0.5, 0.05), xycoords = 'figure fraction',fontsize = 14, weight='bold',horizontalalignment = 'center')
+    plt.annotate(yaxisLabelStr, xy=(0.03, 0.5), xycoords='figure fraction', fontsize=14,
                  weight='bold',rotation = 90, va = 'center')
     plt.tight_layout()
+    plt.subplots_adjust(wspace = 0.3,hspace = 0.75)
     if not os.path.isdir(workingDir + 'Examples'+fileSep):
         os.makedirs(workingDir + 'Examples'+fileSep)
     plt.savefig(workingDir + 'Examples'+fileSep+'Example_Page_'+str(montagePageNumber)+'.png', format = 'png', dpi = 600)
@@ -616,14 +637,14 @@ if sectionNumber == 10:
 
     cmd = (JIM + 'Isolate_Particle' + fileEXE + ' \"' + workingDir + 'Alignment_Channel_To_Channel_Alignment.csv\" \"'
            + workingDir + 'Alignment_Channel_1.csv\" \"' + workingDir + 'Detected_Filtered_Measurements.csv\" \"'
-           + workingDir + 'Examples\Example\" '+allChannelNames+ ' -Start '+str(montageStart)+ ' -End '+str(montageEnd)+ ' -Particle '
-           + str(montageTraceNo)+ ' -Delta '+str(montageDelta)+ ' -Average '+str(montageAverage))
+           + workingDir + 'Examples'+os.sep+'Example\" '+allChannelNames+ ' -Start '+str(montageStart)+ ' -End '+str(montageEnd)+ ' -Particle '
+           + str(montageTraceNo) + ' -Delta '+str(montageDelta)+ ' -Average '+str(montageAverage))
 
     if montageOutputParticleImageStack:
         cmd = cmd + ' -outputImageStack'
     os.system(cmd)
 
-    imName = (workingDir + 'Examples\Example_Trace_' + str(montageTraceNo) + '_Range_' + str(montageStart) + '_' +
+    imName = (workingDir + 'Examples'+os.sep+'Example_Trace_' + str(montageTraceNo) + '_Range_' + str(montageStart) + '_' +
          str(montageDelta) + '_' + str(montageEnd) + '_montage.tiff')
     img = mpimg.imread(imName)
     plt.figure('Trace Montage')
@@ -641,7 +662,7 @@ if sectionNumber == 10:
         data.append(np.array(channelFile).astype(float))
 
     plt.figure(figsize=(4, 3), facecolor=(1, 1, 1))
-    xpoints = np.arange(0.0, len(data[0][0])) * montageTimePerFrame;
+    xpoints = np.arange(0.0, len(data[0][0])) * montageTimePerFrame
 
     for j in range(imStackNumberOfChannels):
         toplot = data[j][montageTraceNo]
@@ -649,6 +670,7 @@ if sectionNumber == 10:
 
     plt.plot(plt.xlim(), [0, 0], color='black')
     plt.ylim(-0.2, 1)
+    plt.xlim(min(xpoints),max(xpoints))
 
     plt.xlabel('Time (' + montageTimeUnits + ')', fontsize=14, weight='bold',ha='center')
     plt.ylabel('Normalised Intensity', fontsize=14,weight='bold', rotation=90, va='center')
@@ -756,7 +778,7 @@ if sectionNumber == 12:
                 cmdIn = cmdIn + ' -Verbose'
             os.system(cmdIn)
 
-        falsetrue = ['false', 'true'];
+        falsetrue = ['false', 'true']
         variableString = ('Date, ' + str(datetime.date.today()) +
                           '\nadditionalExtensionsToRemove,' + str(additionalExtensionsToRemove) +
                           '\nimStackMultipleFiles,' + falsetrue[int(imStackMultipleFiles)] +
@@ -819,3 +841,4 @@ if sectionNumber == 12:
         p = Pool(batchNumberOfThreads)
         p.map(f, np.arange(len(fileList)))
         print('Batch Analysis Complete')
+
