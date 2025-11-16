d8297f2fbaaa909e6b2e6f6b675f1c238b06227a
lilbutsa <james.walsh@phys.unsw.edu.au>
2025-11-17 09:50:37 +1100

    Bump

--- Jim_v8/Matlab_Programs/IndexedStructCopy.m
+++
@@ -1,9 +0,0 @@
-function T = IndexedStructCopy(S, Condition, FieldList)
-if nargin == 2
-   FieldList = fieldnames(S);
-end 
-for iField = 1:numel(FieldList)
-   Field    = FieldList{iField};
-   T.(Field) = S.(Field)(Condition);
-end
-end
